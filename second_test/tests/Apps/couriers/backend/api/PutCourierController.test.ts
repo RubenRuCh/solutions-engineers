2da59d846bc0f15fc549151933ddee9e2881406f
Rubén Rüger <ruben_27895@hotmail.com>
2022-05-05 21:54:56 +0200

    feat: :sparkles: Update courier max-capacity (test E2E)

---
+++ second_test/tests/Apps/couriers/backend/api/PutCourierController.test.ts
@@ -0,0 +1,52 @@
+import httpStatus from 'http-status';
+import request from 'supertest';
+import { InMemoryCapacityTrackCourierRepository } from '../../../../../src/Contexts/CapacityTrack/Courier/Infraestructure/Repository/InMemoryCapacityTrackCourierRepository';
+import { CapacityTrackCourierMother } from '../../../../Contexts/CapacityTrack/Courier/Domain/Model/CapacityTrackCourierMother';
+import app from '../app';
+
+const repository = new InMemoryCapacityTrackCourierRepository();
+
+describe('PutCourierController', () => {
+  beforeEach(async () => {
+    await repository.clear();
+  });
+
+  afterAll(async () => {
+    await repository.clear();
+    app?.close();
+  });
+
+  it('should return 200 if update courier max capacity process is successful', async () => {
+    const courier = CapacityTrackCourierMother.random();
+    await repository.persist(courier);
+
+    await request(app)
+      .put(`/couriers/${courier.id.value}`)
+      .send({
+        max_capacity: courier.maxCapacity.value
+      })
+      .expect(res => {
+        expect(res.status).toEqual(httpStatus.OK);
+        expect(res.body).toEqual({
+          id: courier.id.value,
+          maxCapacity: courier.maxCapacity.value,
+          currentCapacity: courier.maxCapacity.value
+        });
+      });
+  });
+
+  it('should return 400 if trying to put max capacity under 0', async () => {
+    const courier = CapacityTrackCourierMother.random();
+    await repository.persist(courier);
+
+    await request(app)
+      .put(`/couriers/${courier.id.value}`)
+      .send({
+        max_capacity: -10
+      })
+      .expect(res => {
+        expect(res.status).toEqual(httpStatus.BAD_REQUEST);
+        expect(res.body.errorType).toBe('CapacityTrackCourierCapacityMustBeZeroOrPositiveException');
+      });
+  });
+});
