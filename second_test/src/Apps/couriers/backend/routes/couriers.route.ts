63cfbd6488e73ecff61a2098c6fed9c5e924ed4a
Rubén Rüger <ruben_27895@hotmail.com>
2022-05-05 22:00:15 +0200

    Add new endpoint (put courier)

--- second_test/src/Apps/couriers/backend/routes/couriers.route.ts
+++ second_test/src/Apps/couriers/backend/routes/couriers.route.ts
@@ -1,9 +1,11 @@
 import express, { Express, Router } from 'express';
 import { AsyncHandler } from '../../../../Contexts/Shared/Infraestructure/AsyncHandler';
 import { PostCourierController } from '../controllers/couriers/PostCourierController';
+import { PutCourierController } from '../controllers/couriers/PutCourierController';
 
 enum CouriersEndpoints {
-  PostOne = '/'
+  PostOne = '/',
+  PutOne = '/:courierId'
 }
 
 const router: Router = express.Router();
@@ -11,6 +13,11 @@ const router: Router = express.Router();
 router.post(
   CouriersEndpoints.PostOne,
   AsyncHandler((req: Request, res: Response) => new PostCourierController().run(req, res))
+);
+
+router.put(
+  CouriersEndpoints.PutOne,
+  AsyncHandler((req: Request, res: Response) => new PutCourierController().run(req, res))
 );
 
 export const register = (app: Express) => {
